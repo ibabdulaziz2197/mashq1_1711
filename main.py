bc91d990ace8ce8f8ebf6e1462819aa7cd32c331
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2025-11-17 15:23:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+# 1
+i = 'abcdefghj'
+print(i)
+
+toza = i[2:5:1]
+print(toza)
+
+
+# 2
+i = "HelloWerold"
+print(i)
+
+toza = i[0:5]
+print(toza)
+
+# 3
+i = 'PythonPragaraming'
+print(i)
+
+toza = i[-7:]
+print(toza)
+
+# 4
+s = "abcdefighijikl"
+print(s)
+
+toza = s[::-1]
+print(toza)
+
+
+# 5
+s = "0123456789"
+print(s)
+
+toza = s[0:10:2]
+print(toza)
+
+# 6
+i = "WelcomeTopython"
+print(i)
+
+t = i[3:9]
+print(t)
+
+# 7
+i = "PythonLanguage"
+print(i)
+
+t = i[3:10]
+print(t)
+
+# 8
+i = "ABCDEFGHIJKLM"
+print(i)
+
+t = i[5:10]
+print(t)
+
+# 9
+s = "superprogrammer"
+print(s)
+
+t = s[::-4]
+print(t)
+
+10 
+s = "0123456789"
+print(s)
+
+toza = s[0:10:2]
+print(toza)
